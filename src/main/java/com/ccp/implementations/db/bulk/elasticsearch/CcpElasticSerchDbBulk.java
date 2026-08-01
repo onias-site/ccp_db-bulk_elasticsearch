bc9664553b5fbe57b7a4a24eb36221a2eaac4645
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2026-07-31 23:55:00 -0300

    refatoracao completa do login

--- src/main/java/com/ccp/implementations/db/bulk/elasticsearch/CcpElasticSerchDbBulk.java
+++ src/main/java/com/ccp/implementations/db/bulk/elasticsearch/CcpElasticSerchDbBulk.java
@@ -12,7 +12,7 @@ import com.ccp.especifications.db.bulk.CcpBulkExecutor;
  */
 public class CcpElasticSerchDbBulk implements CcpInstanceProvider<CcpBulkExecutor> {
 
-	
+
 	public CcpBulkExecutor getInstance() {
 		ArrayList<CcpBulkItem> bulkItems = new ArrayList<>();
 		return new ElasticSerchDbBulkExecutor(bulkItems);
